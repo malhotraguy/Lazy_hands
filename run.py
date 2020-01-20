1b9361c9777e6e7559a9c924ecb540515bba7c67
malhotraguy <r_malhot@encs.concordia.ca>
2020-01-19 19:52:57 -0500

    change port environment variable

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
 from app import app
 
+load_dotenv(verbose=True)
+
+PORT = os.environ.get("PORT", 5000)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True, use_reloader=True)
